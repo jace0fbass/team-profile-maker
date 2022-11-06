dc7018c4f183ae75a814000294eb5866c8d5e563
Jason <jajones414@gmail.com>
2022-11-06 13:22:11 -0600

    I have added the intern test and created functions to to retrieve role and github. Also added variables to index.js file.

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,13 @@
+const Engineer = require('../lib/Engineer')
+
+it('creates an engineer object and retrieves Github', () => {
+    const engineer = new Engineer('Lauren')
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github))
+})
+
+it('retrieves employee role', () => {
+const employee = new Engineer('Lauren')
+
+expect(employee.getRole()).toBe('Engineer')
+})
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 const generateHTML = require('./src/generateHTML')
-
+const Manager = require('./lib/Manager')
+const Intern = require('./lib/Intern')
+const Engineer = require('./lib/Engineer')
 
 
 
