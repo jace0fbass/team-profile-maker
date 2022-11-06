0c776a80cdb0b373da7cd4ffefc3f7d353113c2c
Jason <jajones414@gmail.com>
2022-11-05 22:40:59 -0500

    I have reorganized the workspace folder to reflect the challenge requirements.

---
+++ index.js
@@ -0,0 +1,21 @@
+import jest from "jest";
+import inquirer from "inquirer";
+
+inquirer.prompt([
+  {
+    type: "checkbox",
+    message: "Select ID",
+    choices: ["Manager", "Employee", "Engineer", "Intern"],
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "Enter full name",
+    name: "name,",
+  },
+  {
+    type: "input",
+    message: "Enter email address",
+    name: "email",
+  },
+]);
