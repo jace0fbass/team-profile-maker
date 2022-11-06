296f7540346a87ac0a2beb62cd31d8eb91eec524
Jason <jajones414@gmail.com>
2022-11-06 12:35:31 -0600

    I have added the manager test and created functions to retrieve role and office.

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,14 @@
+const manager = require('../lib/Manager')
+
+it('creates a manager object and retrieves office number', () => {
+    const manager = new Manager('Jason')
+
+    expect(manager.getOffice()).toEqual(expect.stringContaining(manager.office))
+})
+
+it('gets employee role', () => {
+    const employee = new Manager('Jason')
+
+    expect(employee.getRole).toBe('Manager')
+})
+
