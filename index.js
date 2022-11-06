e261e998282b916b127c752a7ab00adefa79e793
Jason <jajones414@gmail.com>
2022-11-06 14:16:36 -0600

    I have added the intern prompt and added the key value pair of name I forgot to put in secondPrompt.

--- index.js
+++ index.js
@@ -45,10 +45,12 @@ Profile.prototype.startPrompt = function() {
 
 Profile.prototype.secondPrompt = function() {
   inquirer.prompt({
-    type: 'checkbox',
-    message: 'Add more team members',
-    choices: ['Engineer', 'Intern', 'Finish']
+    type: "checkbox",
+    message: "Add more team members",
+    choices: ['Engineer', 'Intern', 'Finish'],
+    name: "name"
   })
+
   .then(({add}) => {
     if(add === 'Engineer') {
       this.EngineerPrompt()
@@ -57,4 +59,35 @@ Profile.prototype.secondPrompt = function() {
     } else {this.writePage()
     }
   })
+}
+
+Profile.prototype.internPrompt = function() {
+  inquirer.prompt(
+    {
+    type: "text",
+    message: "Enter intern's name.",
+    name: "name"
+    },
+    {
+      type: "text",
+      message: "Enter intern's ID number.",
+      name: "id"
+    },
+    {
+      type: "text",
+      message: "Enter intern's email.",
+      name: "email"
+    },
+    {
+      type: "text",
+      message: "Enter intern's school.",
+      name: "school"
+    })
+
+    .then(({ name, id, email, school }) => {
+      this.employee = new Intern(name, id, email, school)
+      this.employee.role = this.employee.getRole()
+      this.employeeArr.push(this.employee)
+      this.secondPrompt()
+    })
 }
