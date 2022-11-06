0d1f884ab4186c6eb4abef6903e7c7e6d2cce3d6
Jason <jajones414@gmail.com>
2022-11-06 13:50:32 -0600

    I have added Profile function and filled out the team manager portion of the prompt then took the enetered data and pushed it to employee.

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
-const generateHTML = require('./src/generateHTML')
-const Manager = require('./lib/Manager')
-const Intern = require('./lib/Intern')
-const Engineer = require('./lib/Engineer')
+const inquirer = require("inquirer")
+const fs = require("fs")
+const generateHTML = require("./src/generateHTML")
+const Manager = require("./lib/Manager")
+const Intern = require("./lib/Intern")
+const Engineer = require("./lib/Engineer")
+const { profile } = require("console")
 
+function Profile() {
+  this.employeeArr = []
+  this.employee
+}
 
+Profile.prototype.startPrompt = function() {
+  inquirer.prompt([
+    {
+      type: "text",
+      message: "Enter team manager's name.",
+      name: "name"
+    },
+    {
+      type: "text",
+      message: "Enter team manager's ID number.",
+      name: "id"
+    },
+    {
+      type: "input",
+      message: "Enter team manager's email address.",
+      name: "email"
+    },
+    {
+      type: "text",
+      message: "Enter team manager's office number.",
+      name: "office"
+    },
+  ])
 
-inquirer.prompt([
-  {
-    type: "checkbox",
-    message: "Select ID",
-    choices: ["Manager", "Employee", "Engineer", "Intern"],
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "Enter full name",
-    name: "name,",
-  },
-  {
-    type: "input",
-    message: "Enter email address",
-    name: "email",
-  },
-]);
+  .then(({ name, id, email, office}) => {
+    this.employee = new Manager(name, id, email, office)
+    this.employee.role = this.employee.getRole()
+    this.employeeArr.push(this.employee)
+  })
+
+};
