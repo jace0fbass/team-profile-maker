7240383df8f94b13aea69f80833bca090a98b642
Jason <jajones414@gmail.com>
2022-11-06 12:13:39 -0600

    Changed the value of test in the package.json to jest to correctly run tests.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,6 @@
 const Employee = require('../lib/Employee')
 
-test('creates an employee object', () => {
+it('creates an employee object', () => {
     const employee = new Employee('Jason', 'jajones414@gmail.com', '0')
 
     expect(employee.name).toBe('Jason')
@@ -8,25 +8,25 @@ test('creates an employee object', () => {
     expect(employee.id).toBe('0')
 })
 
-test("asks for employee's id name", () => {
+it("asks for employee's id name", () => {
     const employee = new Employee('Jason')
 
     expect(employee.getName()).toEqual(expect.stringContaining(employee.name))
 })
 
-test("asks for employee's email", () => {
+it("asks for employee's email", () => {
     const employee = new Employee('Jason')
 
     expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email))
 })
 
-test("asks for employee's id number", () => {
+it("asks for employee's id number", () => {
     const employee = new Employee('Jason')
 
     expect(employee.getId()).toEqual(expect.stringContaining(employee.id))
 })
 
-test("asks for employee's role", () => {
+it("asks for employee's role", () => {
     const employee = new Employee('Jason')
 
     expect(employee.getRole()).toBe('employee')
