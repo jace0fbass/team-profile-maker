da2dd25c3a7a41dd8394c4587ea5a8ba037b6001
Jason <jajones414@gmail.com>
2022-11-08 22:49:27 -0600

    Still working to display the answers on the employee cards. Changed employeeArr to answers.

--- index.js
+++ index.js
@@ -6,32 +6,12 @@ const Intern = require("./lib/Intern.js");
 const Engineer = require("./lib/Engineer.js");
 
 // starts the prompts with manager info
-let employeeArr = [];
+let answers = [];
 
-// starts second prompt
-function secondPrompt() {
-  inquirer
-    .prompt({
-      type: "list",
-      message: "Add more team members",
-      choices: ["Engineer", "Intern", "Finish"],
-      name: "emp",
-    })
-
-    .then(({ emp }) => {
-      if (emp === "Engineer") {
-        engineerPrompt();
-      } else if (emp === "Intern") {
-        internPrompt();
-      } else {
-        writePage();
-      }
-    });
-}
 
 function startPrompt() {
   inquirer
-    .prompt([
+  .prompt([
       {
         type: "text",
         message: "Enter team manager's name.",
@@ -53,18 +33,39 @@ function startPrompt() {
         name: "office",
       },
     ])
-
+    
     // pushes entered info into employee and starts the next prompt
     .then((name, id, email, office) => {
       let employee = new Manager(name, id, email, office);
-      employeeArr.push(employee);
+      answers.push(employee);
       secondPrompt();
     });
-}
-
-// prompts for intern info
-function internPrompt() {
-  inquirer
+  }
+  
+  // starts second prompt
+  function secondPrompt() {
+    inquirer
+      .prompt({
+        type: "list",
+        message: "Add more team members",
+        choices: ["Engineer", "Intern", "Finish"],
+        name: "emp",
+      })
+  
+      .then(({ emp }) => {
+        if (emp === "Engineer") {
+          engineerPrompt();
+        } else if (emp === "Intern") {
+          internPrompt();
+        } else {
+          writePage();
+        }
+      });
+  }
+  
+  // prompts for intern info
+  function internPrompt() {
+    inquirer
     .prompt([
       {
         type: "text",
@@ -90,7 +91,7 @@ function internPrompt() {
 
       .then((name, id, email, school) => {
         let employee = new Intern(name, id, email, school);
-        employeeArr.push(employee);
+        answers.push(employee);
         secondPrompt();
       });
     }
@@ -123,14 +124,14 @@ function engineerPrompt() {
 
       .then((name, id, email, github) => {
         let employee = new Engineer(name, id, email, github);
-        employeeArr.push(employee);
+        answers.push(employee);
         secondPrompt();
       });
 }
 
 // writes generated html page
 function writePage() {
-  const pageHTML = generateHTML(employeeArr);
+  const pageHTML = generateHTML(answers);
   fs.writeFile("./dist/index.html", pageHTML, (err) => {
     if (err) throw err;
   });
